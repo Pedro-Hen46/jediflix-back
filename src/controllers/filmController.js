01b0e42f5626fd97fcf40c1312599cabd1d3ca4d
Pedro Henrique <driverique@gmail.com>
2023-02-04 22:33:02 -0300

    🍿 - Adding more films em database

--- src/controllers/filmController.js
+++ src/controllers/filmController.js
@@ -1,42 +1,4 @@
-const films = [
-    {
-        id: 1,
-        name: "Gemini: O planeta sombrio",
-        overview: "Em um futuro próximo... Após séculos de peregrinação ambiental, os dias da Terra estão contados. Com ela, a espécie humana está ameaçada de extinção. Mas a esperança renasce quando um jovem cientista revela um processo revolucionário de terraformação, que abre caminho para a colonização de novos planetas. Um grupo de elite de todos os matizes e de todas as nacionalidades é formado. Sua missão: assumir o comando da nau capitânia da frota internacional e partir em busca de um novo lar para a humanidade. Navegando rumo ao desconhecido, esses novos exploradores estão longe de imaginar os imensos perigos que os aguardam.",
-        frontCover: "https://br.web.img2.acsta.net/c_310_420/pictures/23/01/04/17/26/0928174.jpg",
-        headerImage: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj5Vp4VE2-vh80bjk5n9e5k_1MWT3jOG9OHnonjGuGmoRg3UFyCB9arQpCJKaHU94CmypzyGdcDhbRLZbmxwqjKBitz4WqzOJUoyIrnP-Mn8BcUqf_FpAx0w7hlyBsduWcqIgywMl0WPmKPDLP6qd_-nTya1QVcbi25o9iaFSht6k6ihd7o0BqyTtdkrQ/w640-h330/Gemini-O-Planeta-Sombrio_Condor-Distribution%20(6).JPG",
-        price: 25.00,
-        category: "Ficção Cientifica, Horror",
-        productedAt: "2022",
-        duration: "1:58",
-        ageClassification: 14
-    },
-    {
-        id: 2,
-        name: "Avatar 2: O caminho da agua",
-        overview: "Em Avatar: O Caminho da Água, sequência de Avatar (2009), após dez anos da primeira batalha de Pandora entre os Na'vi e os humanos, Jake Sully (Sam Worthington) vive pacificamente com sua família e sua tribo. Ele e Ney'tiri formaram uma família e estão fazendo de tudo para ficarem juntos, devido a problemas conjugais e papéis que cada um tem que exercer dentro da tribo. No entanto, eles devem sair de casa e explorar as regiões de Pandora, indo para o mar e fazendo pactos com outros Na'vi da região. Quando uma antiga ameaça ressurge, Jake deve travar uma guerra difícil contra os humanos novamente. Mesmo com dificuldade, Jake acaba fazendo novos aliados - alguns dos quais já vivem entre os Na'vi e outros com novos avatares. Mesmo com uma guerra em curso, Jake e Ney'tiri terão que fazer de tudo para ficarem juntos e cuidar da família e de sua tribo.",
-        frontCover: "https://br.web.img2.acsta.net/c_310_420/pictures/22/05/09/16/16/3197518.jpg",
-        headerImage: "https://veja.abril.com.br/wp-content/uploads/2022/12/FILME-AVATAR-2-O-CAMINHO-DA-AGUA-2022-016.jpg.jpg?quality=70&strip=info&w=1280&h=720&crop=1",
-        price: 30.00,
-        category: "Ficção Cientifica, Fantasia",
-        productedAt: "2022",
-        duration: "3:12",
-        ageClassification: 12
-    },
-    {
-        id: 3,
-        name: "GATO DE BOTAS 2: O ÚLTIMO PEDIDO",
-        overview: "Em Gato de Botas 2: O Último Pedido, O Gato de Botas descobre que sua paixão pela aventura cobrou seu preço: por conta de seu gosto pelo perigo e pelo desrespeito à segurança pessoal, ele queimou oito de suas nove vidas. Com apenas uma vida restante, o Gato precisa pedir ajuda para uma antiga parceira - que atualmente é sua rival e inimiga mortal, Kitty Pata Mansa - para continuar vivo. Então, o destemido bichano parte em uma jornada épica pela Floresta Negra para encontrar a mítica Estrela dos Desejos, capaz de proporcionar o legendário Último Desejo e restaurar suas nove vidas.",
-        frontCover: "https://br.web.img2.acsta.net/c_310_420/pictures/22/12/27/14/37/5994557.jpg",
-        headerImage: "https://s2.glbimg.com/1SMeCXTtxiqZw8UgCJs42TTxp58=/0x0:1920x1080/1008x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2023/j/q/961HATTrqmPLpPlnLEYQ/gato-de-botas-2-o-ultimo-pedido.jpg",
-        price: 20.00,
-        category: "Ação, Comédia, Aventura",
-        productedAt: "2023",
-        duration: "1:42",
-        ageClassification: 0
-    },
-    
-]
+import { films } from "../dbStrategy/database.js";
 
 export async function allFilmsList(req, res){
     res.json(films)
---
+++ src/dbStrategy/database.js
@@ -0,0 +1,132 @@
+export const films = [
+    {
+        id: 1,
+        name: "Gemini: O planeta sombrio",
+        overview: "Em um futuro próximo... Após séculos de peregrinação ambiental, os dias da Terra estão contados. Com ela, a espécie humana está ameaçada de extinção. Mas a esperança renasce quando um jovem cientista revela um processo revolucionário de terraformação, que abre caminho para a colonização de novos planetas. Um grupo de elite de todos os matizes e de todas as nacionalidades é formado. Sua missão: assumir o comando da nau capitânia da frota internacional e partir em busca de um novo lar para a humanidade. Navegando rumo ao desconhecido, esses novos exploradores estão longe de imaginar os imensos perigos que os aguardam.",
+        frontCover: "https://br.web.img2.acsta.net/c_310_420/pictures/23/01/04/17/26/0928174.jpg",
+        headerImage: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj5Vp4VE2-vh80bjk5n9e5k_1MWT3jOG9OHnonjGuGmoRg3UFyCB9arQpCJKaHU94CmypzyGdcDhbRLZbmxwqjKBitz4WqzOJUoyIrnP-Mn8BcUqf_FpAx0w7hlyBsduWcqIgywMl0WPmKPDLP6qd_-nTya1QVcbi25o9iaFSht6k6ihd7o0BqyTtdkrQ/w640-h330/Gemini-O-Planeta-Sombrio_Condor-Distribution%20(6).JPG",
+        price: 25.00,
+        category: "Ficção Cientifica, Horror",
+        productedAt: "2022",
+        duration: "1:58",
+        ageClassification: 14
+    },
+    {
+        id: 2,
+        name: "Avatar 2: O caminho da agua",
+        overview: "Em Avatar: O Caminho da Água, sequência de Avatar (2009), após dez anos da primeira batalha de Pandora entre os Na'vi e os humanos, Jake Sully (Sam Worthington) vive pacificamente com sua família e sua tribo. Ele e Ney'tiri formaram uma família e estão fazendo de tudo para ficarem juntos, devido a problemas conjugais e papéis que cada um tem que exercer dentro da tribo. No entanto, eles devem sair de casa e explorar as regiões de Pandora, indo para o mar e fazendo pactos com outros Na'vi da região. Quando uma antiga ameaça ressurge, Jake deve travar uma guerra difícil contra os humanos novamente. Mesmo com dificuldade, Jake acaba fazendo novos aliados - alguns dos quais já vivem entre os Na'vi e outros com novos avatares. Mesmo com uma guerra em curso, Jake e Ney'tiri terão que fazer de tudo para ficarem juntos e cuidar da família e de sua tribo.",
+        frontCover: "https://br.web.img2.acsta.net/c_310_420/pictures/22/05/09/16/16/3197518.jpg",
+        headerImage: "https://veja.abril.com.br/wp-content/uploads/2022/12/FILME-AVATAR-2-O-CAMINHO-DA-AGUA-2022-016.jpg.jpg?quality=70&strip=info&w=1280&h=720&crop=1",
+        price: 30.00,
+        category: "Ficção Cientifica, Fantasia",
+        productedAt: "2022",
+        duration: "3:12",
+        ageClassification: 12
+    },
+    {
+        id: 3,
+        name: "GATO DE BOTAS 2: O ÚLTIMO PEDIDO",
+        overview: "Em Gato de Botas 2: O Último Pedido, O Gato de Botas descobre que sua paixão pela aventura cobrou seu preço: por conta de seu gosto pelo perigo e pelo desrespeito à segurança pessoal, ele queimou oito de suas nove vidas. Com apenas uma vida restante, o Gato precisa pedir ajuda para uma antiga parceira - que atualmente é sua rival e inimiga mortal, Kitty Pata Mansa - para continuar vivo. Então, o destemido bichano parte em uma jornada épica pela Floresta Negra para encontrar a mítica Estrela dos Desejos, capaz de proporcionar o legendário Último Desejo e restaurar suas nove vidas.",
+        frontCover: "https://br.web.img2.acsta.net/c_310_420/pictures/22/12/27/14/37/5994557.jpg",
+        headerImage: "https://s2.glbimg.com/1SMeCXTtxiqZw8UgCJs42TTxp58=/0x0:1920x1080/1008x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2023/j/q/961HATTrqmPLpPlnLEYQ/gato-de-botas-2-o-ultimo-pedido.jpg",
+        price: 20.00,
+        category: "Ação, Comédia, Aventura",
+        productedAt: "2023",
+        duration: "1:42",
+        ageClassification: 0
+    },
+    {
+        id: 4,
+        name: "Tudo em Todo o Lugar ao Mesmo Tempo",
+        overview: "Uma mulher se envolve em uma aventura onde somente ela pode salvar o mundo.",
+        frontCover: "https://br.web.img3.acsta.net/c_310_420/pictures/22/05/16/17/59/5165498.jpg",
+        headerImage: "https://www.cnnbrasil.com.br/wp-content/uploads/sites/12/2022/06/Tudo-em-Todo-o-Lugar-ao-Mesmo-Tempo.jpg",
+        price: 20.00,
+        category: "Ação, Comédia, Ficção Científica",
+        productedAt: "23 de junho de 2022",
+        duration: "1:42",
+        ageClassification: 12
+    },
+    {
+        id: 5,
+        name: "A Profecia do Mal",
+        overview: "Uma poderosa sociedade secreta que possui uma tecnologia revolucionária que permite clonar as pessoas mais influentes da história com apenas alguns fragmentos de DNA. Por trás desse empreendimento está uma cabala de satanistas que roubam o Sudário de Turim, colocando-os de posse do DNA de Jesus Cristo. O clone servirá como a oferenda definitiva ao diabo.",
+        frontCover: "https://br.web.img3.acsta.net/c_310_420/pictures/22/12/28/18/25/4889872.jpg",
+        headerImage: "https://jornal140.com/wp-content/uploads/2023/01/a-profecia-do-mal-imagem-filmes-cultura-jornal-140.jpg",
+        price: 20.00,
+        category: "Aventura, Terror, Ficção Científica",
+        productedAt: "26 de janeiro de 2023",
+        duration: "1:51",
+        ageClassification: 16
+    },
+
+    {
+        id: 6,
+        name: "Alerta Máximo",
+        overview: "Em Alerta Máximo, o piloto Brodie Torrance (Gerard Butler) salva seus passageiros de um relâmpago fazendo um pouso arriscado em uma ilha devastada pela guerra – e descobre que sobreviver ao pouso era apenas o começo.",
+        frontCover: "https://br.web.img2.acsta.net/c_310_420/pictures/22/10/26/20/33/2949158.png",
+        headerImage: "https://cinepop.com.br/wp-content/uploads/2022/10/alerta-maximo2.webp",
+        price: 20.00,
+        category: "Ação, Suspense",
+        productedAt: "26 de janeiro de 2023",
+        duration: "1:48",
+        ageClassification: 14
+    },
+    
+
+    {
+        id: 7,
+        name: "O Pior Vizinho do Mundo",
+        overview: "Após as cartas de sua nova vizinha parar na caixa de correio do rabugento Otto, uma amizade inesperada se forma e muda a vida dos dois vizinhos.",
+        frontCover: "https://br.web.img2.acsta.net/c_310_420/pictures/22/12/20/20/04/1210526.png",
+        headerImage: "https://grupomobicine.com.br/wp-content/uploads/Banner-PVDM.png",
+        price: 25.00,
+        category: "Comédia, Comédia dramática, Drama",
+        productedAt: "16 de janeiro de 2023",
+        duration: "2:07",
+        ageClassification: 12
+    },
+    
+
+    {
+        id: 8,
+        name: "M3GAN",
+        overview: "Uma roboticista faz um protótipo chamado M3GAN, projetada para ser companheira de qualquer criança do país. Após ganhar custodia de sua sobrinha, ela decide testar o novo robô.",
+        frontCover: "https://br.web.img2.acsta.net/c_310_420/pictures/22/10/11/19/20/4591973.jpg",
+        headerImage: "https://gizmodo.uol.com.br/wp-content/blogs.dir/8/files/2022/12/m3gan2-800x419.webp",
+        price: 10.00,
+        category: "Terror, Suspense",
+        productedAt: "2023",
+        duration: "1:42",
+        ageClassification: 0
+    },
+    
+
+    {
+        id: 9,
+        name: "BATEM À PORTA",
+        overview: "Em Batem à Porta, Eric (Ben Aldrige) e Andrew (Jonathan Groff) estão de férias com sua filha Wen (Kristen Cui) em uma cabana isolada no campo. Enquanto brincava na floresta, o pequeno Wen conhece o educado Leonard (Dave Bautista). A tranquilidade da família então chega a um fim abrupto quando ele, acompanhado por três outros estranhos armados (Rupert Grint, Nikki Amuka-Bird e Abby Quinn), conseguem entrar à força na cabana e faz a família como refém. Os invasores contam a seus reféns sobre uma visão misteriosa e os forçam a tomar uma decisão inimaginável, um sacrifício para evitar o apocalipse iminente. Com acesso limitado ao mundo exterior, a família deve decidir em que acredita antes que tudo se perca: eles realmente pretendem salvar o mundo ou Leonard e seus companheiros fazem parte de um culto insano?",
+        frontCover: "https://br.web.img3.acsta.net/c_310_420/pictures/22/12/28/21/18/5098506.jpg",
+        headerImage: "https://i0.wp.com/cloud.estacaonerd.com/wp-content/uploads/2022/12/25160110/maxresdefault-57.jpg?fit=1280%2C720&ssl=1",
+        price: 20.00,
+        category: "Suspense, Terror",
+        productedAt: "2023",
+        duration: "1:45",
+        ageClassification: 12
+    },
+    
+    {
+        id: 10,
+        name: "O GRANDE MAURICINHO",
+        overview: "Mas ele é, também, um malandro, vendendo suas habilidades felinas para enganar as pessoas que o contratam. Ele não faz isso tudo sozinho. Junto com seu parceiro Kinho, um flautista mágico, eles convocam uma horda de ratos e os levam para fora da cidade.",
+        frontCover: "https://br.web.img3.acsta.net/c_310_420/pictures/23/01/04/16/23/0261774.jpg",
+        headerImage: "https://www.aconteceempetropolis.com.br/wp-content/uploads/2023/02/o-grande-mauricinho.jpg",
+        price: 25.00,
+        category: "Animação, Fantasia, Aventura, Família",
+        productedAt: "2023",
+        duration: "1:33",
+        ageClassification: 0
+    },
+    
+    
+]
